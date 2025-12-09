70536fb1d0907dd0bb5ea24e5a0857da6e84b36b
daan <idaany@gmail.com>
2025-12-09 22:08:40 +0100

    Fix plugin registry and add Vue devtools fix

--- panel/index.js
+++ panel/index.js
@@ -1,9 +1,18 @@
 import { usePanel  } from "kirbyuse";
-const panel = usePanel;
+const panel = usePanel();
 
 // For now we only provide modules which you can import.
 // Register the plugin name (empty) anyways.
 
 panel.plugin("daandelange/helpers", {
-    
+    use: {
+        // See https://github.com/getkirby/kirby/issues/4796#issuecomment-1285304129
+        // Helps vue devtools detecting Fiber's Vue instance
+        plugin(Vue) {
+            // Check for VueDevtools Browser plugin
+            if (panel.debug && window.__VUE_DEVTOOLS_GLOBAL_HOOK__) {
+                window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = Vue;
+            }
+        },
+    },
 });
